943ec8732ff67068e5ab20869f43722defaeae35
andre_luiz_amorim <andre_luiz_amorim@SNSOO1EP01VM079.saojose.sc.senai.br>
2015-11-12 16:06:42 -0200

    Arquivos projeto mercadinho

--- editar.php
+++ editar.php
@@ -1,4 +1,20 @@
 <?php include_once ("template/global.php"); ?>
 <?php include_once ("template/header.php"); ?>
-
+<table>
+    <tr>
+        <td>nome</td>
+        <td>valor</td>
+        <td>qtd</td>
+        <td>validade</td>
+        <td></td>
+    </tr>
+    <?php
+    if($_post["editar"]){
+        editar2($_post["editar"]);
+    }
+    if(isset($_post["name"]) and isset($_post["valor"]) and isset($_post["qtd"]) and isset($_post["validade"]) and isset($_post["validade"])){
+        editar($_post["nome"], $_post["valor"], $_post["qtd"], $_post["validade"], $_post["editar"]);
+    }
+    ?>
+</table>
 <?php include_once ("template/footer.php"); ?>
